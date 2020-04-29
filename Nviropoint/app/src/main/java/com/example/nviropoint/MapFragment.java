a013c86961505e63afe8336fc8b5cd8887ab968a
10749683 <57794344+10749683@users.noreply.github.com>
2020-04-29 14:21:54 +0100

    29/04/2020
    -Settings page
    -Notifications
    -Logo on icon
    -New Splashscreen

---
+++ Nviropoint/app/src/main/java/com/example/nviropoint/MapFragment.java
@@ -0,0 +1,52 @@
+package com.example.nviropoint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * The type Map fragment.
+ */
+public class MapFragment extends Fragment {
+
+    /**
+     * Instantiates a new Map fragment.
+     */
+//String url = "https://maps.openweathermap.io/map/temp_new/256/256/256.json?appid=faf15c72035a522d6e027c2be057069c";
+    public MapFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //MainActivity.makeJsonArrayRequest();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFrg);
+
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        });
+
+        return rootView;
+    }
+
+
+}
---
+++ Nviropoint/app/src/main/java/com/example/nviropoint/SettingsFragment.java
@@ -0,0 +1,29 @@
+package com.example.nviropoint;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+
+    public SettingsFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
